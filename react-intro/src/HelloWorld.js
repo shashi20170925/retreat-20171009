b8cd7991b9c619e35e80ee750efc962cf7f840c8
Shashi Thipparthy <shashi_thipparthy@gallup.com>
2017-10-08 13:23:17 -0500

    Added Hello World component

---
+++ react-intro/src/HelloWorld.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+          <h1 className="App-title">Welcome to React</h1>
+
+      </div>
+    );
+  }
+}
+
+export default App;
--- react-intro/src/index.js
+++ react-intro/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import HelloWorld from './HelloWorld';
+
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
