4722416112514ef8de3d3036d048090f2fb6b9b4
Shashi Thipparthy <shashi_thipparthy@gallup.com>
2017-10-08 23:10:59 -0500

    props

--- react-intro/src/components/Content.js
+++ react-intro/src/components/Content.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import UserDetails from './UserDetails';
 class Content extends Component {
   render() {
     return (
       <div>
           <h1>Content Component</h1>
+          <UserDetails userName={"Gary"}/>
       </div>
     );
   }
---
+++ react-intro/src/components/UserDetails.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class UserDetails extends Component {
+  //Will get props from the parent 
+  render() {
+    return (
+      <div>
+          <h1>UserDetails Component </h1>
+          <br/>
+          <p>{this.props.userName}</p>
+      </div>
+    );
+  }
+}
+export default UserDetails;
