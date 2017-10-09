4944a13f63d76006de276e9b123f75ec99a87d8d
Shashi Thipparthy <shashi_thipparthy@gallup.com>
2017-10-08 22:56:14 -0500

    formatting

--- react-intro/src/components/Content.js
+++ react-intro/src/components/Content.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-
 class Content extends Component {
   render() {
     return (
       <div>
           <h1>Content Component</h1>
-
       </div>
     );
   }
 }
-
 export default Content;
